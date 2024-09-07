86e483523f47da1bea476d0e5448c8c1cdb23aa9
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-09-07 14:43:27 +0530

    first commit

---
+++ Basic.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE information;
+use information
+
+CREATE TABLE student_details (
+    student_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    age INT,
+    gender VARCHAR(10),
+    department VARCHAR(100),
+    gpa DECIMAL(3, 2),
+    enrollment_year INT
+);
+
+
+INSERT INTO student_details (student_id, first_name, last_name, age, gender, department, gpa, enrollment_year)
+VALUES
+(1, 'Rohith', 'Kumar', 22, 'Male', 'Computer Science', 3.8, 2021),
+(2, 'Alan', 'Walker', 23, 'Male', 'Mechanical Engineering', 3.6, 2020),
+(3, 'Sara', 'Smith', 21, 'Female', 'Data Science', 3.9, 2022),
+(4, 'Vikas', 'Sharma', 24, 'Male', 'Information Technology', 3.5, 2019),
+(5, 'Priya', 'Rao', 22, 'Female', 'Electrical Engineering', 3.7, 2021);
+
+SELECT * from student_details
